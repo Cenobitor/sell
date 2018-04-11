822b449ee41c6633b672ba18997f5d2e3dc0e700
gdw <1037345628@qq.com>
2018-04-11 21:24:13 +0800

    Initial commit

---
+++ src/test/java/com/cenobitor/sell/LoggerTest.java
@@ -0,0 +1,28 @@
+package com.cenobitor.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.impl.SLF4JLog;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+    private SLF4JLog log;
+
+    @Test
+    public void test1() {
+        String name = "imooc";
+        String password = "123456";
+        log.debug("debug...");
+        log.info("name: " + name + " ,password: " + password);
+//        log.info("name: {}, password: {}", name, password);
+        log.error("error...");
+        log.warn("warn...");
+    }
+}
